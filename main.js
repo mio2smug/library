e5fa859f2ef337829ebc8cb6a915a806cf0b6d20
mio2smug <bryan.lazarowork@yahoo.com>
2023-02-23 14:06:08 -0500

    JS code

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 /* eslint-disable semi */
 console.log('Hello')
+
+//book object
+const books{
+	title: 'book title';
+	author: 'book author';
+	pages: 0;
+	isRead: false;
+}
+
+//constructor
+function newBook(title, author, pages, isRead){
+	this.title = title
+	this.author = author
+	this.pages = pages
+	this isRead = isRead
+}
+
+
+let book1 = new newBook('Grapes of Wrath', 'John Steinbeck', 464, true)

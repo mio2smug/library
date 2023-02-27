738c5c7f580049aeae9cce9d649b5b67213c8ed2
mio2smug <bryan.lazarowork@yahoo.com>
2023-02-27 14:44:03 -0500

    JS receies inputs from HTML & CSS works

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ const span = document.getElementById('mySpan');
 const buttonSubmit = document.getElementById('submit--button');
 
 //  JS constants
+const myLibrary = [];
+
+// object constructor
+function book (title, author, pages, bool) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.bool = bool
+}
 
 //  ___proto____
 buttonAdd.onclick = function () {
@@ -16,11 +25,18 @@ span.onclick = function () {
 };
 
 buttonSubmit.onclick = function () {
+  addBookToLibrary();
   modal.style.display = 'none';
 };
 
-
-
+function addBookToLibrary() {
+  let title = document.getElementById('book--title').value
+  let author = document.getElementById('author--name').value
+  let pages = document.getElementById('pages').value
+  let checkbox = document.getElementById('isRead').checked
+  let newbook = new book(title, author, pages, checkbox)
+  myLibrary.push(newbook)
+}
 // window.onclick = function(e) {
 //   if (e.target == modal) {
 //     modal.style.display = 'none';
